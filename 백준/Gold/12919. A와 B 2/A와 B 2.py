e2446b1fe884f393d0cb0f06fdcd94c1a6d80a6f
Song <84127378+et007693@users.noreply.github.com>
2025-06-09 19:27:07 +0900

    [Gold V] Title: A와 B 2, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.py
+++ 백준/Gold/12919. A와 B 2/A와 B 2.py
@@ -1,21 +1,17 @@
-from collections import deque
-
 s = input()
 t = input()
-q = deque([t])
-result = 0
 
-while q:
-    string = q.popleft()
+def dfs(t):
+    if len(t) == len(s):
+        if t == s:
+            print(1)
+            exit()
+        else:
+            return
+    if t[-1] == "A":
+        dfs(t[:-1])
+    if t[0] == "B":
+        dfs(t[::-1][:-1])
 
-    if string == s:
-        result = 1
-        break
-    
-    if len(string) > 1:
-        if string[-1] == "A":
-            q.append(string[:-1])
-        if string[0] == "B":
-            q.append(string[::-1][:-1])
-
-print(result)
+dfs(t)
+print(0)
