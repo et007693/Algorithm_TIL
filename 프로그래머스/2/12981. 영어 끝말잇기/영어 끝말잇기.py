287590397fb0371fdc66dead22ed520504a10082
Song <84127378+et007693@users.noreply.github.com>
2025-06-13 10:23:05 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,9 +1,7 @@
 def solution(n, words):
     stack = []
-    
     for i, word in enumerate(words):
         if word in stack or (stack and stack[-1][-1] != word[0]):
-            
             return [(i % n) + 1, (i // n) + 1]
         else:
             stack.append(word)
