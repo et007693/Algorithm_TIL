17f4180a98d48056c7d44e85a1654c7eb7b2ef9d
Song <84127378+et007693@users.noreply.github.com>
2025-06-14 21:11:12 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 1.16 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,12 +1,8 @@
 from collections import deque
 
 def solution(food):
-    q = deque([0])
+    answer = '0'
     for f in range(len(food)-1, 0, -1):
         for _ in range(food[f]//2):
-            q.appendleft(f)
-            q.append(f)
-    answer = ''
-    for i in q:
-        answer += str(i)
+            answer = f'{f}{answer}{f}'
     return answer
