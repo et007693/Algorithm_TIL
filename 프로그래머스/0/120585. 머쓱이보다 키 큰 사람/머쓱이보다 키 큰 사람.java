200d6dd9d0a85aab7c47885a1e9fc9f1b5542dcd
Song <84127378+et007693@users.noreply.github.com>
2025-12-16 17:01:27 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.34 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array, int height) {
+        int cnt = 0;
+        Arrays.sort(array);
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > height) break;
+            cnt++;
+        }
+        return array.length - cnt;
+    }
+}
